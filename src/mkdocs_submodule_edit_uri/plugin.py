eaca5b84a7251913f3c29bc003c34835a1c2b882
Sondre Grønås <44143748+sondregronas@users.noreply.github.com>
2022-08-16 17:00:34 +0200

    Add ' - ' edge case

--- src/mkdocs_submodule_edit_uri/plugin.py
+++ src/mkdocs_submodule_edit_uri/plugin.py
@@ -16,6 +16,6 @@ class SubmoduleEditUriPlugin(BasePlugin):
     def on_post_page(self, output, page, config):
         for v in self.config['tuples']:
             old = v['old'][:-len(v['old'].split('/')[-1])] + page.url[:-1]
-            new = v['new']+page.url[len(page.url.split('/')[0]):].replace('-', '%20')[:-1]
+            new = v['new']+page.url[len(page.url.split('/')[0]):].replace('-', ' ').replace('   ', ' - ')[:-1]
             output = output.replace(old, new)
         return output

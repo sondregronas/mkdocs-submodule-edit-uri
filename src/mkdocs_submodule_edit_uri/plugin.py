6e041e505e4c5de7036a27a8d7a66449f6dddedf
Sondre Grønås <44143748+sondregronas@users.noreply.github.com>
2022-08-16 17:48:47 +0200

    Update plugin.py

--- src/mkdocs_submodule_edit_uri/plugin.py
+++ src/mkdocs_submodule_edit_uri/plugin.py
@@ -16,8 +16,12 @@ class SubmoduleEditUriPlugin(BasePlugin):
     def on_post_page(self, output, page, config):
         separator = '-'
         for v in self.config['tuples']:
-            old = v['old'] + page.url[len(v['old'].split('/')[-1]):-1].replace('%20', ' ')
-            new = v['new'] + page.url[len(page.url.split('/')[0]):-1].replace(separator, '%20').replace('%20%20%20', f'%20{separator}%20')
+            old = v['old'] + page.url[len(v['old'].split('/')[-1]):-1]
+            # Sanitize old
+            old = old.replace('%20', ' ').replace('%2C', ',')
+            new = v['new'] + page.url[len(page.url.split('/')[0]):-1]
+            # Sanitize new
+            new = new.replace(separator, '%20').replace('%20%20%20', f'%20{separator}%20')
 
             output = output.replace(f'{old}/index.md', f'{new}/index.md')
             output = output.replace(f'{old}.md', f'{new}.md')
